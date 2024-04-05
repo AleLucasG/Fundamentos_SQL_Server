150d31680bb9d8137ac1d67ce9660c0ef8a50b20
Alessandra Lucas <alessandra1702@hotmail.com>
2024-04-04 21:51:34 -0300

    Add files via upload

--- CURSO_SQL_BALTA_OI_II.sql
+++ CURSO_SQL_BALTA_OI_II.sql
@@ -1,17 +1,5 @@
 
-
-DROP TABLE TB_CATEGORIA;
-
---CURSO
-CREATE TABLE TB_CURSO(
-       Id INT NOT NULL,
-       Nome VARCHAR2(60) NOT NULL,
-       CategoriaId INT NOT NULL,
-       
-       CONSTRAINT PK_CURSO PRIMARY KEY (Id),
-       CONSTRAINT FK_CURSO_CATEGORIA FOREIGN KEY (CategoriaId) REFERENCES TB_CATEGORIA(Id));
-     
------------------------------------------------------------
+DROP TABLE TB_CURSO;
 
 -- 1º Crie a sequência
 CREATE SEQUENCE categoria_seq
@@ -19,15 +7,18 @@ START WITH 1
 INCREMENT BY 1;
 
 -- 2º Crie a tabela com a coluna ID utilizando a sequência
-CREATE TABLE TB_CATEGORIA (
-    Id INT NOT NULL,
-    Nome VARCHAR2(60) NOT NULL,
-    CONSTRAINT PK_CATEGORIA PRIMARY KEY (Id)
+CREATE TABLE TB_CURSO(
+       Id INT NOT NULL,
+       Nome VARCHAR2(60) NOT NULL,
+       CategoriaId INT NOT NULL,
+       
+       CONSTRAINT PK_CURSO PRIMARY KEY (Id),
+       CONSTRAINT FK_CURSO_CATEGORIA FOREIGN KEY (CategoriaId) REFERENCES TB_CATEGORIA(Id));
 );
 
 -- 3º  Crie um gatilho para inserir automaticamente valores na coluna ID
-CREATE OR REPLACE TRIGGER tb_categoria_trigger
-BEFORE INSERT ON TB_CATEGORIA
+CREATE OR REPLACE TRIGGER tb_curso_trigger
+BEFORE INSERT ON TB_CURSO
 FOR EACH ROW
 BEGIN
     SELECT categoria_seq.NEXTVAL INTO :NEW.Id FROM dual;
@@ -36,11 +27,12 @@ END;
 
 --4º Insert com auto incremento na coluna ID
 
-INSERT INTO TB_CATEGORIA (Nome) VALUES('BackEnd');
-INSERT INTO TB_CATEGORIA (Nome) VALUES('FrontEnd');
-INSERT INTO TB_CATEGORIA (Nome) VALUES('Mobile');
-INSERT INTO TB_CATEGORIA (Nome) VALUES('Desktop');
+
+INSERT INTO TB_CURSO (Nome, CategoriaId) VALUES ('Fundamentos C#', 21);
+INSERT INTO TB_CURSO (Nome, CategoriaId) VALUES ('Fundamentos OOP', 21);
+INSERT INTO TB_CURSO (Nome, CategoriaId) VALUES ('Angular', 22);
+INSERT INTO TB_CURSO (Nome, CategoriaId) VALUES ('Flutter', 23);
+INSERT INTO TB_CURSO (Nome, CategoriaId) VALUES ('Windows Form', 24);
 
        
-SELECT * FROM TB_CATEGORIA;
-
+SELECT * FROM TB_CURSO;
