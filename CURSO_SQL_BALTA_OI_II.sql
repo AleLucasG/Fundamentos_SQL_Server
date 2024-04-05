215ec1a14c94af91c7523658c42022a914cc3fce
Alessandra Lucas <alessandra1702@hotmail.com>
2024-04-04 21:38:01 -0300

    Auto Incremento ID
    Correção para auto incremento na coluna ID

--- CURSO_SQL_BALTA_OI_II.sql
+++ CURSO_SQL_BALTA_OI_II.sql
@@ -1,20 +1,6 @@
 
-DROP TABLE TB_CURSO;
 
-
---CATEGORIA
-CREATE TABLE TB_CATEGORIA(
-       Id INT NOT NULL,
-       Nome VARCHAR2(60) NOT NULL,
-       
-       CONSTRAINT PK_CATEGORIA PRIMARY KEY (Id)
-);
-
-INSERT INTO TB_CATEGORIA (Id, Nome) VALUES(1, 'BackEnd');
-INSERT INTO TB_CATEGORIA (Id, Nome) VALUES(2, 'FrontEnd');
-INSERT INTO TB_CATEGORIA (Id, Nome) VALUES(3, 'Mobile');
-
-
+DROP TABLE TB_CATEGORIA;
 
 --CURSO
 CREATE TABLE TB_CURSO(
@@ -23,6 +9,38 @@ CREATE TABLE TB_CURSO(
        CategoriaId INT NOT NULL,
        
        CONSTRAINT PK_CURSO PRIMARY KEY (Id),
-       CONSTRAINT FK_CURSO_CATEGORIA FOREIGN KEY (CategoriaId) REFERENCES TB_CATEGORIA(Id)); --so pode ser criada, quando a TB_CATEGORIA existir.
+       CONSTRAINT FK_CURSO_CATEGORIA FOREIGN KEY (CategoriaId) REFERENCES TB_CATEGORIA(Id));
+     
+-----------------------------------------------------------
+
+-- 1º Crie a sequência
+CREATE SEQUENCE categoria_seq
+START WITH 1
+INCREMENT BY 1;
+
+-- 2º Crie a tabela com a coluna ID utilizando a sequência
+CREATE TABLE TB_CATEGORIA (
+    Id INT NOT NULL,
+    Nome VARCHAR2(60) NOT NULL,
+    CONSTRAINT PK_CATEGORIA PRIMARY KEY (Id)
 );
+
+-- 3º  Crie um gatilho para inserir automaticamente valores na coluna ID
+CREATE OR REPLACE TRIGGER tb_categoria_trigger
+BEFORE INSERT ON TB_CATEGORIA
+FOR EACH ROW
+BEGIN
+    SELECT categoria_seq.NEXTVAL INTO :NEW.Id FROM dual;
+END;
+/
+
+--4º Insert com auto incremento na coluna ID
+
+INSERT INTO TB_CATEGORIA (Nome) VALUES('BackEnd');
+INSERT INTO TB_CATEGORIA (Nome) VALUES('FrontEnd');
+INSERT INTO TB_CATEGORIA (Nome) VALUES('Mobile');
+INSERT INTO TB_CATEGORIA (Nome) VALUES('Desktop');
+
+       
+SELECT * FROM TB_CATEGORIA;
 
